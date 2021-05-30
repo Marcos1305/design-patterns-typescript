c78812ecd863fe9157815ae58db8c834ed1bc4fa
Marcos Vinicius <marcos1305pt@gmail.com>
2021-05-30 19:34:29 -0300

    factory pattern

---
+++ src/creational/factory-method/vehicle/client-code.ts
@@ -0,0 +1,12 @@
+import { randomVehicleAlgorithm } from '../main/random-vehicle-algorithm';
+import { randomNumbers } from '../utils/random-numbers';
+
+const customerNames = ['Ana', 'Joana', 'Helena', 'João'];
+
+for (let i = 0; i < 10; i++) {
+  const vehicle = randomVehicleAlgorithm();
+  const name = customerNames[randomNumbers(customerNames.length)];
+  vehicle.pickUp(name);
+  vehicle.stop();
+  console.log('----');
+}
